9468761f5ba1a53d8b3eaa8797ca5a8c2a3554cc
Vanessa Schneider <vanessajwade@gmail.com>
2022-05-11 19:06:32 -0400

    changes

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -8,11 +8,14 @@ class Rating < ApplicationRecord
       show_id = self.show_id
       show = Show.where(id:show_id)
       all_ratings = Rating.where(show_id:show_id)
-      if all_ratings.length < 2
+      if all_ratings.length ==1
         the_ratings = all_ratings.first
         show.update(rating: the_ratings.rating)
         show.update(rating_number:1)
       else
+        show_id = self.show_id
+      show = Show.where(id:show_id)
+      all_ratings = Rating.where(show_id:show_id)
       my_ratings = all_ratings.average(:rating).to_i
       show.update(rating: my_ratings)
       show.update(rating_number: "#{all_ratings.length}")
