8256a97ae0ba92f82930bc7e7fe5abb570f09fab
Vanessa Schneider <vanessajwade@gmail.com>
2022-05-11 19:37:49 -0400

    trying to fix bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
       if user.valid?
-        user.make_data
           render json: user, status: :created
       else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
