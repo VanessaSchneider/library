c5ac0b9e88de87094f81e9145baad69ff0685151
Vanessa Schneider <vanessajwade@gmail.com>
2022-06-01 22:42:18 -0400

    edited

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // import Login from './Login.js'
-// import { useState, useEffect } from 'react'
+ import { useState, useEffect } from 'react'
 // import Signup from './Signup'
 // import Logout from './Logout.js'
 import { Route, Switch, useHistory, Link, useLocation } from 'react-router-dom'
@@ -7,12 +7,19 @@ import BookDisplay from './BookDisplay'
 
 
 function App () {
+  const [books, setBooks] = useState("");
+  useEffect(() => {
+    fetch("/books")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
 
 
   return (
     <div>
-    <div className = "centered">Writers have a hard time getting their work out there. But here at Kissed Every Frog Novels, we give authors the chance to get out there!</div> 
-    <BookDisplay/>
+    <div className = "centered">Writers have a hard time getting their work out there. But here, we give authors the chance to get out there!</div> 
+    <br></br>
+    <BookDisplay books={books}/>
      </div>
   )
 
--- client/src/BookDisplay.js
+++ client/src/BookDisplay.js
@@ -1,23 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-function BookDisplay () {
-    const [books, setBooks]=useState("")
-    useEffect(() => {
-        fetch('/books')
-          .then(res => res.json())
-          .then(data => setBooks(data))
-      }, [])
-    
-      console.log(books)
+function BookDisplay({books}) {
+  
+
+  console.log(books);
+
+let book = []
+if (books && books.length!==0){
+ book = 
+    books.map((book) => (
+    <div key={book.id}>
+      <div>{book.name}</div>
+      <div>{book.summary}</div>
+    </div>
+  ));}
 
 
-
-
-    return(
-        <div>
-        <div className = "centered"> Book Display </div>
-        </div>
-    )
-
+  return (
+    <div>
+      <div className="centered"> {book} </div>
+    </div>
+  );
 }
 export default BookDisplay;
