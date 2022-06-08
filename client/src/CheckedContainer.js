c17aea96bffefa6f470b0bd643f8b6c0a1b6b041
Vanessa Schneider <vanessajwade@gmail.com>
2022-06-08 12:54:59 -0400

    user page

--- client/src/CheckedContainer.js
+++ client/src/CheckedContainer.js
@@ -4,14 +4,24 @@ import { Link, useHistory } from 'react-router-dom'
 function CheckedContainer({book}) {
 
 
+function returnBook(){
 
+
+
+}
 
 
   return (
+<div className = "catTile2">
    <div className = "centered2">
        {book.name}
+       <br></br>
        {book.author}
        <img img className='book-size' src={book.photo}></img>
+       <br></br>
+       <br></br>
+       <button onClick={returnBook}>Return Book</button>
+   </div>
    </div>
   )
 }
